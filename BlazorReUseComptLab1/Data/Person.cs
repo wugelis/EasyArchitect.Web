427f426a47d9989956c5055e8d5c7bc806069c11
wugelis <wergelis@msn.com>
2023-11-03 15:28:17 +0800

    (1).GridView增加GetData方法

--- BlazorReUseComptLab1/Data/Person.cs
+++ BlazorReUseComptLab1/Data/Person.cs
@@ -10,5 +10,9 @@ namespace BlazorReUseComptLab1.Data
         public string CustomerId { get; set; }
         public string ContactName { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
     }
 }
--- EasyArchitect.Web/Blazor/Components/GridView.razor.cs
+++ EasyArchitect.Web/Blazor/Components/GridView.razor.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EasyArchitect.Web.Blazor.Components
@@ -23,6 +25,11 @@ namespace EasyArchitect.Web.Blazor.Components
             base.OnParametersSet();
         }
 
+        /// <summary>
+        /// 取得在 Response OutputStream 中的資料列（傳入的資料應該是強型別）
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
         string GetDataRow(object row)
         {
             string result = string.Empty;
@@ -35,6 +42,40 @@ namespace EasyArchitect.Web.Blazor.Components
                 result += $"<td>{p.GetValue(row)}</td>";
             }
             result += "</tr>";
+
+            return result;
+        }
+
+        /// <summary>
+        /// 根據 startURL 取得 Response OutputStream
+        /// </summary>
+        /// <param name="_startUrl"></param>
+        /// <returns></returns>
+        public static async Task<object> GetData<T>(string _startUrl)
+        {
+            object result = null;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, _startUrl);
+
+            HttpClient _client = new HttpClient();
+
+            _client.BaseAddress = new Uri(_startUrl);
+
+            var client = _client;
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+
+                result = await JsonSerializer.DeserializeAsync<IEnumerable<T>>(
+                    responseStream,
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
 
             return result;
         }
